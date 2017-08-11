2347b6ad066aa6ab7dfb02d43357a5d471516eb6
faafzal <faafzal@outlook.com>
2017-08-10 23:09:39 -0700

    Update Database.java

--- Model/Database.java
+++ Model/Database.java
@@ -21,62 +21,44 @@ public class Database{
 	 * Open a connection to the sqlite database
 	 */
 	public void openConn(){
-		
 		try{
-			
-		    Class.forName("org.sqlite.JDBC").newInstance();
-		    this.conn = DriverManager.getConnection("jdbc:sqlite:libraryDB.db");
-			
+			Class.forName("org.sqlite.JDBC").newInstance();
+			this.conn = DriverManager.getConnection("jdbc:sqlite:libraryDB.db");
 		}catch(Exception e){
-			
-		     System.out.println("Error: " + e);
-		     System.exit(0);
-			
+			System.out.println("Error: " + e);
+			System.exit(0);
 		}
-		
 	}
 	
 	/*
 	 * Close the connection
 	 */
 	public void closeConn(){
-		
 		try{
-			
-		    this.conn.close();
-			
+			this.conn.close();
 		} catch (SQLException e) {
-			
-		      System.out.println("Error: " + e);
-		      System.exit(0);
-			
+			System.out.println("Error: " + e);
+			System.exit(0);
+
 		}
-		
 	}
 	
 	/*
 	 * Delete a table
 	 */
 	public void delete(String table){
-		
-	    this.openConn();
-		
-	    try {
+		this.openConn();
 
-	      Statement state = this.conn.createStatement();
-	      String del = "DROP TABLE "+table+";";
-	      state.executeUpdate(del);
-	      state.close();
-	      
-	    } catch ( Exception e ) {
-	    	
-	      System.out.println("Error: " + e);
-	      System.exit(0);
-	      
-	    }
-	    
-	    this.closeConn();
-		
+		try {
+			Statement state = this.conn.createStatement();
+			String del = "DROP TABLE "+table+";";
+			state.executeUpdate(del);
+			state.close();
+		} catch ( Exception e ) {
+			System.out.println("Error: " + e);
+			System.exit(0);
+		}
+
+		this.closeConn();	
 	}
-
 }
