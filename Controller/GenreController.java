41d1d13a4be810e532a1e2d73aedc0c1c11c1956
faafzal <faafzal@outlook.com>
2017-08-10 23:32:42 -0700

    Update GenreController.java

--- Controller/GenreController.java
+++ Controller/GenreController.java
@@ -21,7 +21,6 @@ public class GenreController{
 	private BooksTable booksTable;
 	
 	public GenreController(MainFrame mf, User u){
-		
 		//store the main frame that is passed through
 		this.mainFrame = mf;
 		//store user object that is passed through
@@ -38,61 +37,51 @@ public class GenreController{
 		this.mainFrame.add(this.genrePanel.getSouthPanel(), BorderLayout.SOUTH);
 		//revalidate the main frame
 		this.mainFrame.revalidate();
-		
 	}
 	
 	/*
 	 * This method is called when the Choose Button is clicked
 	 */
 	public void onChoose(){
-		
 		//make sure user selected a genre
 		if(this.genrePanel.getGenreList().getSelectedValue() != null){
-			
 			//remove panels
 			this.mainFrame.remove(this.genrePanel.getMainPanel());
 			this.mainFrame.remove(this.genrePanel.getSouthPanel());
 			
 			//call Controller for the Books Panel
 			new BooksController(this.mainFrame, this.loggedUser, this.genrePanel.getGenreList().getSelectedValue().toString());
-		
-		}
-		
+		}	
 	}
 	
 	/*
 	 * This method is called when the Profile Button is clicked
 	 */
 	public void onProfile(){
-		
 		//remove panels
 		this.mainFrame.remove(this.genrePanel.getMainPanel());
 		this.mainFrame.remove(this.genrePanel.getSouthPanel());
 		
 		//call Controller for the Profile Panel
-		new ProfileController(this.mainFrame, this.loggedUser);
-		
+		new ProfileController(this.mainFrame, this.loggedUser);	
 	}
 	
 	/*
 	 * This method is called wehn the Logout Button is clicked
 	 */
 	public void onLogout(){
-		
 		//remove panels
 		this.mainFrame.remove(this.genrePanel.getMainPanel());
 		this.mainFrame.remove(this.genrePanel.getSouthPanel());
 		
 		//call Controller for Home Panel
 		new HomeController(this.mainFrame);
-		
 	}
 
 	/*
 	 * Helper Class For Action Events
 	 */
 	private class Listener implements ActionListener{
-
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			// TODO Auto-generated method stub
@@ -103,23 +92,12 @@ public class GenreController{
 			 * Check to see which button is clicked and call the appropriate method
 			 */
 			if(btn == genrePanel.getChooseBtn()){
-				
 				onChoose();
-				
 			}else if(btn == genrePanel.getProfileBtn()){
-				
 				onProfile();
-				
 			}else if(btn == genrePanel.getLogoutBtn()){
-				
 				onLogout();
-				
 			}
-			
 		}
-	
-		
 	}
-	
-	
 }
